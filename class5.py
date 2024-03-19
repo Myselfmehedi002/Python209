d3bafb6c2c360fe20464d45b7cffdb9a37b8b96a
mehedialif002 <mhalif002@gmail.com>
2024-03-19 13:11:53 +0600

    first commit

---
+++ class5.py
@@ -0,0 +1,45 @@
+# i = 0
+# while i < 20:
+#   print(i)
+#   i += 5
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#   if x == "banana":
+#     break
+
+
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for z in fruits:
+#   if z == "apple":
+#     continue
+#   print(z)
+
+
+
+# for x in range(0,10,2):
+#   print(x)
+
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+
+# for x in adj:
+#   for y in fruits:
+#     print(x, y)
+
+
+arr=[0]*5
+i=0
+while i<5:
+    arr[i]=i
+    i=i+1
+print(arr)
